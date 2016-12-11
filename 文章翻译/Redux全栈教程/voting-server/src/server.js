0355e89632917d573cc3f030627d0328789681d0
stormqx <451045642@qq.com>
2016-12-11 21:34:29 +0800

    fix some error about server.js

--- 文章翻译/Redux全栈教程/voting-server/src/server.js
+++ 文章翻译/Redux全栈教程/voting-server/src/server.js
@@ -5,15 +5,16 @@
 import Server from 'socket.io';
 
 
-export default function startServer() {
+export default function startServer(store) {
     const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
 
-store.subscribe(
-    () => io.emit('state', store.getState.toJS())
-);
-
-io.on('connection', (socket) => {
-    socket.emit('state', store.getState().toJS());
-    socket.on('action', store.dispatch.bind(store));
-});
