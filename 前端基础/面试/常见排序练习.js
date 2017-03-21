0fc4f3a1d86a457226700a95c32d6f51367dc68d
stormqx <451045642@qq.com>
2017-03-21 17:59:31 +0800

    排序算法lian xi

---
+++ 前端基础/面试/常见排序练习.js
@@ -0,0 +1,106 @@
+/**
+ * Created on 21/03/2017.
+ */
+
+
+class Sort {
+    constructor() {
+        this.data = [2,5,3,-1,6,9,0,123,43];
+    }
+
+    executeSort() {
+        console.log('测试数据集:', this.data);
+        this.insertSort(this.data);
+        this.shellSort(this.data);
+        console.log('quickSort结果：', this.quickSort(this.data));
+        console.log('mergeSort结果：', this.mergeSort(this.data));
+
+    }
+
+    // 插入排序
+    insertSort(array) {
+        if(!array.length) return ;
+        for(var i=1; i<array.length; i++) {
+            var temp = array[i];
+            for(var j=i-1; temp < array[j]&&j>=0; j--) {
+                array[j+1] = array[j];
+            }
+            array[j+1] = temp;
+        }
+        console.log('插入排序结果：', array);
+        return array;
+    }
+
+    // 希尔排序
+    shellSort(array) {
+        if(!array.length) return ;
+        for(var incre =array.length; incre > 0; incre=parseInt(incre/2)) {
+            for(var i=incre; i<array.length; i++) {
+                var temp = array[i];
+                for(var j=i-incre; temp<array[i]&&j>=9; j-=incre) {
+                    array[j+incre] = array[j];
+                }
+                array[j+incre] = temp;
+            }
+        }
+        console.log('shellsort结果：', array);
+        return array;
+    }
+
+    // 快速排序
+    quickSort(array) {
+    if (array.length < 2) { return array; }
+
+    var pivot = array[Math.round(array.length / 2)];
+
+    return this.quickSort(array.filter(x => x <  pivot))
+        .concat(array.filter(x => x == pivot))
+        .concat(this.quickSort((array.filter(x => x >  pivot))));
+    };
+
+    merge(left, right, arr) {
+        var a = 0;
+
+        while (left.length && right.length) {
+            arr[a++] = (right[0] < left[0]) ? right.shift() : left.shift();
+        }
+        while (left.length) {
+            arr[a++] = left.shift();
+        }
+        while (right.length) {
+            arr[a++] = right.shift();
+        }
+    }
+
+    mergeSort(arr) {
+        var len = arr.length;
+        if (len === 1) { return; }
+
+        var mid = Math.floor(len / 2),
+            left = arr.slice(0, mid),
+            right = arr.slice(mid);
+
+        this.mergeSort(left);
+        this.mergeSort(right);
+        this.merge(left, right, arr);
+    }
+
+}
+
+// 给Array.prototype添加quick_sort方法
+Array.prototype.quick_sort = function () {
+    if (this.length < 2) { return this; }
+
+    var pivot = this[Math.round(this.length / 2)];
+
+    return this.filter(x => x <  pivot)
+        .quick_sort()
+        .concat(this.filter(x => x == pivot))
+        .concat(this.filter(x => x >  pivot).quick_sort());
+};
+
+
+
+
+var sort = new Sort();
+sort.executeSort();
